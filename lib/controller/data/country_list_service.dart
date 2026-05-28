ac115997d6b17fb4bd1565efb9b2d555815ba330
mgbhone556 <mgbhone556@gmail.com>
2026-05-28 10:19:24 +0630

    update service

--- lib/controller/data/country_list_service.dart
+++ lib/controller/data/country_list_service.dart
@@ -1,13 +1,15 @@
 import 'package:dio/dio.dart';
-
 import '../../model/all_country_list.dart';
 import '../const/api_const.dart';
 
 class CountryListService {
-  Dio _dio = Dio();
+  final Dio _dio = Dio();
 
   Future<List<AllCountryListModel>> getAllCountryList() async {
-    Uri uri = Uri.pase('{$ApiConst.baseUrl}/${ApiConst.all}');
+    Uri uri = Uri.parse('${ApiConst.baseUrl}/${ApiConst.all}');
     final response = await _dio.getUri(uri);
+    return (response.data as List)
+        .map((data) => AllCountryListModel.fromJson(data))
+        .toList();
   }
 }
